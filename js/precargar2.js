1734bc272ff4c2dcab8cb7e557027d44dca2b332
Esteban Canela <estebancanela@hotmail.com>
2017-04-19 18:53:06 -0300

    Se ha agregado el 2do drone
    Se ha cargado el segundo estilo de drone disponible

---
+++ js/precargar2.js
@@ -0,0 +1,32 @@
+$( "#precargado2" ).click(function() {
+
+    //Scrollea hasta el cuadro correspondiente
+    $('html, body').animate({
+        scrollTop: $("#menuDeArmado").offset().top
+    }, 500);
+
+    var myjson =$.getJSON("json/precargado2.json", function(json) {
+        console.log(json);
+        //si logre entrar
+
+        var marco=json.marco;
+        cargar(marco,"#imgMarco");
+        var motor= json.motor;
+        cargar(motor,"#imgMotor");
+        var bateria = json.bateria; cargar(bateria,"#imgBateria");
+        var camara = json.camara;
+        cargar(camara,"#imgCamara");
+        var gps = json.GPS;
+        cargar(gps,"#imgGps");
+        var mando = json.radioreceptor;
+        cargar(mando,"#imgMando");
+    });
+});
+
+
+
+function cargar(modelo,i,j){
+    $(i).attr('src',modelo.imagen);
+
+
+}
