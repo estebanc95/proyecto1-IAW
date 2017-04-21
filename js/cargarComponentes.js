f33d4d95993863fc35404deb7413f3777a7e3a65
German Herrou <german.herrou@gmail.com>
2017-04-21 16:52:39 -0300

    Ahora el hover se aplica a los elementos ya seleccionados.

---
+++ js/cargarComponentes.js
@@ -0,0 +1,55 @@
+function cargarComponentes(idMotor, idMarco, idControl, idBateria, idCamara, idGps){
+
+  var myjson =$.getJSON("json/datosDron.json", function(json) {
+
+      var motor= json.motores[idMotor];
+      atributos="Modelo:"+motor.modelo+
+              " - Revoluciones: "+motor.revoluciones+
+              " - Voltaje "+motor.voltaje;
+      cargar(motor,"#imgMotor",atributos);
+
+      var marco=json.marcos[idMarco];
+      atributos="Modelo: "+marco.modelo+
+              " - Marca: "+marco.marca+
+              " - Color: "+marco.color+
+              " - Material: "+marco.material+
+              " - Número de helices: "+marco.numeroHelices;
+      cargar(marco,"#imgMarco",atributos);
+
+      var bateria = json.baterias[idBateria];
+      atributos="Modelo: "+bateria.modelo+
+              " - Voltaje: "+bateria.voltaje+
+              " - Duración: "+bateria.duracion+
+              " - Tipo: "+bateria.tipo;
+      cargar(bateria,"#imgBateria",atributos);
+
+      var camara = json.camaras[idCamara];
+      atributos="Modelo: "+camara.modelo+
+              " - Marca: "+camara.marca+
+              " - Megapixeles: "+camara.megapixeles;
+      cargar(camara,"#imgCamara",atributos);
+
+      var gps = json.gps[idGps];
+      atributos="Nombre: "+gps.nombre+
+              " - Modelo: "+gps.modelo;
+      cargar(gps,"#imgGps",atributos);
+
+      var mando = json.controles[idControl];
+      atributos="Modelo: "+mando.modelo+
+              " - Plataforma: "+mando.plataforma+
+              " - Frecuencia: "+mando.frecuencia;
+      cargar(mando,"#imgControl",atributos);
+
+      $('[data-toggle="tooltip"]').tooltip();
+
+
+  });
+
+
+}
+
+function cargar(modelo,i,atributos){
+    $(i).attr('src',modelo.imagen);
+    $(i).attr('title',atributos);
+
+}
