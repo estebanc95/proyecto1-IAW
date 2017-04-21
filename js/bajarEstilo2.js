df24f0e00546394ec9833eb1cf32a3a2829e4e76
Esteban Canela <estebancanela@hotmail.com>
2017-04-21 17:19:43 -0300

    Estilo 2
    El estilo 2 esta funcionando

---
+++ js/bajarEstilo2.js
@@ -0,0 +1,11 @@
+$('.mouseBaja').on('click','#bajarMouse', function(){
+    $('html, body').animate({
+        scrollTop: $(".textoArmado2").offset().top
+    }, 500);
+});
+
+$('.video-container').on('click', function(){
+    $('html, body').animate({
+        scrollTop: $(".textoArmado2").offset().top
+    }, 500);
+});
