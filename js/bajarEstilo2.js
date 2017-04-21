b5752acf3a0855d2a44fba1e49c53e551a03bf5e
Esteban Canela <estebancanela@hotmail.com>
2017-04-21 17:49:09 -0300

    Cambio scroll estilo 2

--- js/bajarEstilo2.js
+++ js/bajarEstilo2.js
@@ -1,12 +1,12 @@
 $('.mouseBaja').on('click','#bajarMouse', function(){
     $('html, body').animate({
-        scrollTop: $(".textoArmado2").offset().top
+        scrollTop: $("#menuDeArmado").offset().top
     }, 500);
 });
 
 $('.video-container').on('click', function(){
     $('html, body').animate({
-        scrollTop: $(".textoArmado2").offset().top
+        scrollTop: $("#menuDeArmado").offset().top
     }, 500);
 });
 
@@ -14,6 +14,6 @@ $('.video-container').on('click', function(){
 
 $('.textIntro2').on('click','.textoIntro2', function(){
     $('html, body').animate({
-        scrollTop: $(".textoArmado2").offset().top
+        scrollTop: $("#menuDeArmado").offset().top
     }, 500);
 });
